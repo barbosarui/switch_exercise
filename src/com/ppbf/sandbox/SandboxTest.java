4a7be2c6b7c9b953b7c696bb453b7ed7367992ee
Rui Costa <rui.costa@blip.pt>
2018-02-13 11:51:16 +0000

    update

--- src/com/ppbf/sandbox/SandboxTest.java
+++ src/com/ppbf/sandbox/SandboxTest.java
@@ -1,22 +1,32 @@
 package com.ppbf.sandbox;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class SandboxTest {
 
     @org.junit.Test
     public void ex1() throws Exception {
-
+        assertEquals(Sandbox.ex1(new ArrayList<>(), 1), null);
     }
 
     @org.junit.Test
     public void ex2() throws Exception {
+        assertEquals(Sandbox.ex2(new ArrayList<>()), null);
 
     }
 
     @org.junit.Test
-    public void ex3() throws Exception {
+    public void ex3_1() throws Exception {
+        assertEquals(Sandbox.ex3_1(new ArrayList<>(), new BigDecimal("1.1")), null);
+    }
 
+    @org.junit.Test
+    public void ex3_2() throws Exception {
+        assertEquals(Sandbox.ex3_2(new ArrayList<>(), new HashMap<>()), null);
     }
 
 }
